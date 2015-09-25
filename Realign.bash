59a6f49c9c2cf150d5db19245d3c9e53e037f804
Kevin Luk <kevin@midashrmg6.library.mcgill.ca>
2015-09-25 18:48:20 -0400

    finished realignment script and added parallization support:

--- Realign.bash
+++ Realign.bash
@@ -1,11 +1,26 @@
 #!/bin/bash
 
 ls ../Data/Dedup * | grep _dedup.bam$ > temp.txt
-mkdir ../Data/RealignmentTargets
+
+#mkdir ../Data/RealignmentTargets
+#mkdir ../Data/RealignedBams
 
 while read dedup; do
-	name=${dedup#
-done< temp.txt
+	name=${dedup%_dedup.bam}
+	java -jar ./GenomeAnalysisTK.jar -nt 24 \
+		-T RealignerTargetCreator \
+		-R ../Data/human_g1k_v37.fasta \
+		-I ../Data/Dedup/${dedup}
+		-known ../Data/Mills_and_1000G_gold_standard.indels.b37.sites.vcf \
+		-o ../Data/RealignmentTargets/${name}_realignment_targets.list
+	
+	java -jar ./GenomeAnalysisTK.jar \ 
+    		-T IndelRealigner \ 
+    		-R ../Data/human_g1k_v37.fasta \ 
+    		-I ../Data/Dedup/${dedup} \ 
+    		-targetIntervals ../Data/RealignmentTargets/${name}_realignment_targets.list \ 
+    		-known ../Data/Mills_and_1000G_gold_standard.indels.b37.sites.vcf \ 
 
+done< temp.txt
 
 rm temp.txt
